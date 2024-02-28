adbac7c9726e44f945c4e4ba2b034920b965a4e4
Aris Arisandi <idnasirasira@gmail.com>
2024-02-28 16:46:30 +0700

    Update fillable Employee

--- app/Models/Employee.php
+++ app/Models/Employee.php
@@ -8,4 +8,18 @@ use Illuminate\Database\Eloquent\Model;
 class Employee extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'first_name',
+        'last_name',
+        'middle_name',
+        'address',
+        'zip_code',
+        'date_of_birth',
+        'date_hired',
+        'country_id',
+        'state_id',
+        'city_id',
+        'department_id',
+    ];
 }
